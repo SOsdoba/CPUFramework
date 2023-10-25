f1e7c92ce8824cbb64de502cc191f9e81e134f89
sosdoba <saraosdoba1@gmail.com>
2023-10-24 21:36:14 -0400

    HW 18 changes

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -24,6 +24,7 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(SqlCommand cmd)
         {
+            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
             using (SqlConnection conn = new(SQLUtility.ConnectionString))
             {
@@ -38,22 +39,7 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(string sqlstatement)
         {
-            Debug.Print(sqlstatement);
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-
-            SetAllColumnsAllowNull(dt);
-            
-
-            return dt;
+            return GetDataTable(new SqlCommand(sqlstatement));
         }
 
         public static void ExecuteSQL(string sqlstatement)
