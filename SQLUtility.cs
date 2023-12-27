551f3e142493543776f576fbd7b5c3e9d308b0a9
sosdoba <saraosdoba1@gmail.com>
2023-12-26 19:49:57 -0500

    Changes to SQL Utility session 21

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -22,8 +22,11 @@ namespace CPUFramework
             }
             return cmd;
         }
-
         public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            return DoExecuteSQL(cmd, true);
+        }
+        private static DataTable DoExecuteSQL(SqlCommand cmd, bool loadtable)
         {
             DataTable dt = new();
             using (SqlConnection conn = new(SQLUtility.ConnectionString))
@@ -34,12 +37,19 @@ namespace CPUFramework
                 try
                 {
                     SqlDataReader dr = cmd.ExecuteReader();
-                    dt.Load(dr);
+                    if(loadtable == true)
+                    {
+                        dt.Load(dr);
+                    }
                 }
                 catch(SqlException ex)
                 {
                     string msg = ParseConstraintMsg(ex.Message);
                     throw new Exception(msg);
+                }
+                catch(InvalidCastException ex)
+                {
+                    throw new Exception(cmd.CommandText + ": " + ex.Message,ex);
                 }
                 
             }
@@ -49,7 +59,7 @@ namespace CPUFramework
 
         public static DataTable GetDataTable(string sqlstatement)
         {
-            return GetDataTable(new SqlCommand(sqlstatement));
+            return DoExecuteSQL(new SqlCommand(sqlstatement), true);
             //Debug.Print(sqlstatement);
             //DataTable dt = new();
             //SqlConnection conn = new();
@@ -67,10 +77,27 @@ namespace CPUFramework
 
             //return dt;
         }
+        public static void ExecuteSQL(SqlCommand cmd)
+        {
+            DoExecuteSQL(cmd, false);
+        }
 
         public static void ExecuteSQL(string sqlstatement)
         {
             GetDataTable(sqlstatement);
+        }
+
+        public static void SetParamValue(SqlCommand cmd, string paramname, object value)
+        {
+            try
+            {
+                cmd.Parameters[paramname].Value = value;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(cmd.CommandText + ": " + ex.Message,ex);
+            }
+            
         }
 
         private static string ParseConstraintMsg(string msg)
@@ -86,7 +113,11 @@ namespace CPUFramework
                     prefix = "u_";
                     msgend = " must be unique.";
                 }
-                else if(msg.Contains("fk_"))
+                if(msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+                else if (msg.Contains("fk_"))
                 {
                     prefix = "fk_";
                 }
@@ -107,6 +138,15 @@ namespace CPUFramework
                     msg = msg.Substring(0, pos);
                     msg = msg.Replace("_", " ");
                     msg = msg + msgend;
+
+                    if (prefix == "f_" || prefix == "fk_")
+                    {
+                        var words = msg.Split(" ");
+                        if(words.Length > 1)
+                        {
+                            msg = $"Cannot delete {words[0]} because it has a related {words[1]} record.";
+                        }
+                    }
                 }
             }
             return msg;
