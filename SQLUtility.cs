eb6b5731d1adc9132dc275c09fd43de513aafc84
sosdoba <saraosdoba1@gmail.com>
2024-05-31 15:36:56 -0400

    HW 27 Login and Security

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -8,8 +8,27 @@ namespace CPUFramework
 {
     public class SQLUtility
     {
-        public static string ConnectionString = "";
+        private static string ConnectionString = "";
 
+        public static void SetConnectionString(string connstring, bool tryopen,string userid = "", string password = "")
+        {
+            ConnectionString = connstring;
+            if(userid != "")
+            {
+                SqlConnectionStringBuilder b = new();
+                b.ConnectionString = ConnectionString;
+                b.UserID = userid;
+                b.Password = password;
+                ConnectionString = b.ConnectionString;
+            }
+            if(tryopen)
+            {
+                using(SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                }
+            }
+        }
         public static SqlCommand GetSQLCommand(string sprocname)
         {
             SqlCommand cmd;
@@ -305,6 +324,7 @@ namespace CPUFramework
 
             if (cmd.Connection != null)
             {
+                //sb.AppendLine($"--{cmd.Connection.ConnectionString}");
                 sb.AppendLine($"--{cmd.Connection.DataSource}");
                 sb.AppendLine($"use {cmd.Connection.Database}");
                 sb.AppendLine("go");
